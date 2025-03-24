3f0710ea3522d1d0faf6ffd870c317c1ba77d2f9
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-24 21:48:57 +0000

    add fruit form

--- src/mobile/app/(tabs)/index.tsx
+++ src/mobile/app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import BaseView from '@/components/BaseView';
+import FruitForm from '@/components/custom/FruitForm';
 import { Box } from '@/components/ui/box';
 import { Heading } from '@/components/ui/heading';
 
 export default function HomeScreen() {
   return (
     <BaseView>
-      <Box className='flex-1 justify-center items-center'>
+      <Box className='flex-1 justify-center items-center w-full'>
         <Heading size="2xl">Welcome</Heading>
+        <FruitForm  />
       </Box>
     </BaseView>
   );
