276024eec8093ea2cd25943eaa0083205abd40b8
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-24 22:36:20 +0000

    add deletion

--- src/backend/src/main.py
+++ src/backend/src/main.py
@@ -1,36 +1,62 @@
-from typing import Union
-
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException, status
+from pydantic import BaseModel
+import debugpy
+
+# Enable debugpy listener
+if os.getenv("DEBUGPY_ENABLED", "true").lower() == "true":
+    print("Starting Debugpy...")
+    debugpy.listen(("0.0.0.0", 5678))
 
 app = FastAPI()
+
+MONGO_DETAILS = "mongodb://db:27017"
+client = AsyncIOMotorClient(MONGO_DETAILS)
+db = client["fruits_db"]  # Database name
+
+# Collections for payslips and contracts
+fruits_collection = db.get_collection("fruits")
+
+origins = [
+    "http://localhost",
+    "http://localhost:5175",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Fruit(BaseModel):
+    name: str
+    color: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/fruits/")
-def read_fruits():
-    fruit1 = {
-        "id": 1,
-        "name": "apple",
-        "color": "red"
-    }
-    fruit2 = {
-        "id": 2,
-        "name": "banana",
-        "color": "yellow"
-    }
-    fruit3 = {
-        "id": 3,
-        "name": "orange",
-        "color": "orange"
-    }
-    return [fruit1, fruit2, fruit3]
+@app.get("/fruits")
+async def read_fruits():
+    fruits = await fruits_collection.find({}).to_list()
+    parsed_fruits = [Fruit(**fruit) for fruit in fruits]
+    return parsed_fruits
  
 @app.get("/fruits/{fruit_id}")
-def read_fruit(fruit_id: int):
-    return {
-        "id": fruit_id,
-        "name": "apple",
-        "color": "red"
-    } 
+def read_fruit(fruit_id: str):
+    return Fruit(name="apple", color="red") 
+
+@app.post("/fruits", status_code=status.HTTP_201_CREATED)
+async def create_fruit(fruit: Fruit):
+    await fruits_collection.insert_one(dict(fruit))
+    return fruit
+
+@app.delete("/fruits/{fruit_id}")
+async def delete_fruit(fruit_id: str):
+    await fruits_collection.delete_many({})
+    return {"message": "All fruits deleted"}
