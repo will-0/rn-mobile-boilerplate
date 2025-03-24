60cdc67d22cdbcfcf38c8f38b6a7a177c106d6cd
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-24 21:26:20 +0000

    simple fruits UI

--- src/mobile/app/(tabs)/explore.tsx
+++ src/mobile/app/(tabs)/explore.tsx
@@ -1,12 +1,29 @@
 import BaseView from '@/components/BaseView';
 import { Box } from '@/components/ui/box';
+import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
+import { useGetFruitQuery, useGetFruitsQuery } from '@/state/api';
 
 export default function ExploreScreen() {
+
+  const {
+    data: fruits,
+  } = useGetFruitsQuery();
+
+
   return (
     <BaseView>
-      <Box className='flex-1 justify-center items-center'>
-        <Text>Exploration...</Text>
+      <Box className='flex-1 justify-center items-center gap-4'>
+        <Heading size="2xl">Fruits</Heading>
+        <Heading size="xs">(Yeah, baby)</Heading>
+        {
+          fruits?.map((fruit) => (
+            <Box key={fruit.id} className='bg-gray-100 p-4 rounded-lg flex flex-row gap-4'>
+              <Text className='text-primary-300'>{fruit.name}</Text>
+              <Text className='text-secondary-900'>{fruit.color}</Text>
+            </Box>
+          ))
+        }
       </Box>
     </BaseView>
   );
--- src/mobile/app/_layout.tsx
+++ src/mobile/app/_layout.tsx
@@ -9,6 +9,8 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Provider } from 'react-redux';
+import store from '@/state';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -30,12 +32,14 @@ export default function RootLayout() {
   }
 
   return (
-    <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <Provider store={store}>
+      <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider></GluestackUIProvider>
+    </Provider>
   );
 }
